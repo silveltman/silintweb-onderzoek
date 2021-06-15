615b1e881149551177e4063d8b14b9bcdca35fbe
silveltman <52583398+silveltman@users.noreply.github.com>
2021-06-15 14:18:03 +0200

    Initial commit

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // docs: https://github.com/bjankord/stylelint-config-sass-guidelines
+  extends: [
+    'stylelint-config-sass-guidelines'
+  ],
+
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'order/properties-alphabetical-order': null,
+    'color-hex-case': null,
+    'color-named': null
+  }
+}
